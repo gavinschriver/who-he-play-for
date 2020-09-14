317407c276ae86cf5c9b8c2ef747aa021ff5294d
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-14 10:23:33 -0500

    THING ON A PAGE

---
+++ src/components/WhoHePlayFor.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./WhoHePlayFor.css"
+
+export const WhoHePlayFor = () => (
+    <>
+        <h2>WHO HE PLAY FOR</h2>
+        <small>Forreal, who he play for</small>
+        <address>
+            <div>GOOO rockets</div>
+            <div>Doing great</div>
+        </address>
+    </>
+)
