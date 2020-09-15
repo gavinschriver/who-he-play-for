43c84e9f5177d502cecd693f68ea7d4107908f2f
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-15 14:11:33 -0500

    on to da list

---
+++ src/components/messages/MessagesList.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const MessagesList = () => {
+
+
+    return (
+        <section className="messagesList">
+            <h2>Gonna be some messages here</h2>
+
+        </section>
+    )
+}
