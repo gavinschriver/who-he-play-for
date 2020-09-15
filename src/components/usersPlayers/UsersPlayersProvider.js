25a76de3eeebb03ad7f895713a03a3cf5308c881
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-15 02:37:19 -0500

    usersplayers provider

---
+++ src/components/usersPlayers/UsersPlayersProvider.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, createContext } from "react";
+
+export const UserPlayerContext = React.createContext();
+
+export const CustomerProductProvider = (propsObj) => {
+  const [usersPlayers, setUsersPlayers] = useState([]);
+
+  const getUsersPlayers = () => {
+    return fetch(`http://localhost:8888/usersPlayers`)
+      .then((res) => res.json())
+      .then(setUsersPlayers);
+  };
+
+  const addUserPlayer = (newUserPlayer) => {
+    return fetch("http://localhost:8888/usersPlayers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUserPlayer),
+    }).then(getUsersPlayers);
+  };
+
+  const removeUserPlayer = (userPlayerId) => {
+    return fetch(`http://localhost:8888/usersPlayers/${userPlayerId}`, {
+      method: "DELETE",
+    }).then(getUsersPlayers);
+  };
+    
+    return (
+        <UserPlayerContext.Provider value={{ usersPlayers, getUsersPlayers, addUserPlayer, removeUserPlayer }}>
+            {propsObj.children}
+        </UserPlayerContext.Provider>
+)
+};
