9e7a85bd989a9f1b766a0c0182d2ea0bb8b4ad8e
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-15 13:49:31 -0500

    timmeee to make a provider

--- src/components/messages/MessageEntryForm.js
+++ src/components/messages/MessageEntryForm.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export const MessageEntryForm = () => {
-  const handleSubmitButtonEvent = () => {};
+  const messagetextRef = useRef("");
+  const urlRef = useRef("");
 
+    const handleSubmitButtonEvent = () => {
+        const newMessage = {
+            userId: parseInt(localStorage.getItem("whpf_user")),
+            messagetext: messagetextRef.current.value,
+            url: urlRef.current.value,
+            timestamp: Date.now()
+        }
+        console.log(newMessage)
+  };
   return (
     <section class="messageEntryForm">
       <form>
         <fieldset>
           <input
-                      type="text"
+            type="text"
             name="messagetext"
             id="messagetext"
             placeholder="TALK THAT TRASH"
+            ref={messagetextRef}
             required
           />
           <input
@@ -22,6 +33,7 @@ export const MessageEntryForm = () => {
             pattern="https://.*"
             size="30"
             required
+            ref={urlRef}
           ></input>
         </fieldset>
         <button
