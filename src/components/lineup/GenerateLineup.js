8455e831e2324c6dab5aa9ad8eea350a37db2a29
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-15 01:38:11 -0500

    noowww were talkin

--- src/components/lineup/GenerateLineup.js
+++ src/components/lineup/GenerateLineup.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect, useContext } from "react";
 import { PlayerContext } from "../players/PlayerProvider";
 
 export const GenerateLineup = () => {
-  const {
-    getPlayerData,
-    playerObjArray,
-  } = useContext(PlayerContext);
+  const { getPlayerData, playerObjArray } = useContext(PlayerContext);
 
-    const [lineupShowing, setLineUpShowing] = useState(false);
-    
-    const filteredPlayers = playerObjArray.filter(p => p.player.currentRosterStatus === "ROSTER" && p.player.officialImageSrc) || []
+  const [lineupShowing, setLineUpShowing] = useState(false);
+
+  const filteredPlayers =
+    playerObjArray.filter(
+      (p) =>
+        p.player.currentRosterStatus === "ROSTER" && p.player.officialImageSrc
+    ) || [];
 
   const handleGenerateLineup = () => {
     if (!lineupShowing) {
@@ -20,12 +21,6 @@ export const GenerateLineup = () => {
   useEffect(() => {
     getPlayerData();
   }, []);
-
-//     useEffect(() => {
-//         if (playerData.players) {
-//             setPlayerObjArray(playerData.players)
-//         };
-//   }, [playerData]);
 
   return (
     <>
@@ -39,12 +34,10 @@ export const GenerateLineup = () => {
       </button>
       {lineupShowing ? (
         <section>
-                  <h2>Today's Lineup:</h2>
-                  {
-                      filteredPlayers.map(p => {
-                          return <div>{p.player.currentRosterStatus}</div>
-                      })
-                  }
+          <h2>Today's Lineup:</h2>
+          {filteredPlayers.map((p) => {
+            return <div>{p.player.currentRosterStatus}</div>;
+          })}
         </section>
       ) : (
         <div></div>
