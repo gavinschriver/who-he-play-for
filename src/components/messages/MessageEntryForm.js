71c890a9ca1de64b811a524e68143ce081367978
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-17 18:39:34 -0500

    validation working for stanning

--- src/components/messages/MessageEntryForm.js
+++ src/components/messages/MessageEntryForm.js
@@ -14,62 +14,72 @@ export const MessageEntryForm = () => {
   const messagetextRef = useRef("");
   const urlRef = useRef("");
 
-  const handleSubmitButtonEvent = () => {
-    //does the value of the input for the player's name match a string in the current users collection of player strings and is there something in the URL field?
-    if (matchingPlayersStrings.includes(messagetextRef.current.value)) {
-      const matchingPO = matchingPlayersObjects.find((mPO) => {
-        return mPO.player.firstName === messagetextRef.current.value;
-      });
+  const handleStanButtonPress = () => {
+    const stanplayer = messagetextRef.current.value;
+    const urlValue = urlRef.current.value
+    if (allMatchingPlayersStrings.includes(stanplayer) && urlValue !== '') {
+      if (filteredPlayersStrings.includes(stanplayer)) {
+        const matchingPO = filteredPlayersObjects.find((mPO) => {
+          return mPO.player.firstName === messagetextRef.current.value;
+        });
 
-      const matchingUPO = filteredUsersPlayers.find((uPO) => {
-        return uPO.playerId === matchingPO.player.id;
-      });
+        const matchingUPO = filteredUsersPlayers.find((uPO) => {
+          return uPO.playerId === matchingPO.player.id;
+        });
 
-      const updatedUPO = {
-        id: matchingUPO.id,
-        userId: matchingUPO.userId,
-        playerId: matchingUPO.playerId,
-        mentioned: true,
-      };
+        const updatedUPO = {
+          id: matchingUPO.id,
+          userId: matchingUPO.userId,
+          playerId: matchingUPO.playerId,
+          mentioned: true,
+        };
 
-      updateUserPlayer(updatedUPO);
+        updateUserPlayer(updatedUPO);
 
-      const newMessage = {
-        userId: parseInt(localStorage.getItem("whpf_user")),
-        messagetext: messagetextRef.current.value,
-        url: urlRef.current.value,
-        timestamp: Date.now(),
-        stan: true
-      };
-      addMessage(newMessage);
-      // if not, does the value of the input for the player's name match a string in the collection of strings of other user's players?
-
-    } else {
-      if (othersPlayersStrings.includes(messagetextRef.current.value)) {
-        if (filteredUsersPlayers.length > 0) {
-          const newMessage = {
-            userId: parseInt(localStorage.getItem("whpf_user")),
-            messagetext: messagetextRef.current.value,
-            url: urlRef.current.value,
-            timestamp: Date.now(),
-            trashtalk: true
-          };
-          addMessage(newMessage);
-          console.log(filteredUsersPlayers)
-        } else {alert('nice trick')}
-
-      } else {
         const newMessage = {
           userId: parseInt(localStorage.getItem("whpf_user")),
           messagetext: messagetextRef.current.value,
           url: urlRef.current.value,
           timestamp: Date.now(),
+          stan: true,
         };
         addMessage(newMessage);
+      } else if (allMatchingPlayersStrings.includes(stanplayer)) {
+        alert(`Woah slow down stanimal, you already repped this player`);
       }
-      // alert("no go bro");
-    }
+    } else alert(`better check that input stanley`)
+    console.log(urlValue.length)
   };
+
+  //   const handleSubmitButtonEvent = () => {
+  //     const stanplayer = messagetextRef.current.value
+  //     //does the value of the input for the player's name match a string in the current users collection of player strings and is there something in the URL field?
+  //  else {
+  //       if (othersPlayersStrings.includes(messagetextRef.current.value)) {
+  //         if (filteredUsersPlayers.length > 0) {
+  //           const newMessage = {
+  //             userId: parseInt(localStorage.getItem("whpf_user")),
+  //             messagetext: messagetextRef.current.value,
+  //             url: urlRef.current.value,
+  //             timestamp: Date.now(),
+  //             trashtalk: true
+  //           };
+  //           addMessage(newMessage);
+  //           console.log(filteredUsersPlayers)
+  //         } else {alert('nice trick')}
+
+  //       } else {
+  //         const newMessage = {
+  //           userId: parseInt(localStorage.getItem("whpf_user")),
+  //           messagetext: messagetextRef.current.value,
+  //           url: urlRef.current.value,
+  //           timestamp: Date.now(),
+  //         };
+  //         addMessage(newMessage);
+  //       }
+  //       // alert("no go bro");
+  //     }
+  //   };
 
   useEffect(() => {
     setMentionedCount(
@@ -81,6 +91,24 @@ export const MessageEntryForm = () => {
     );
   }, [usersPlayers]);
 
+  // this colleciton is current user's WHOLE lineup as UPOs
+  const allMatchingUsersPlayers = usersPlayers.filter((upo) => {
+    return upo.userId === parseInt(localStorage.getItem("whpf_user"));
+  });
+
+  // this collection is current user's WHOLE linup as player objects
+  const allMatchingPlayersObjects = allMatchingUsersPlayers.map((fUPO) => {
+    return playerObjArray.find((p) => {
+      return p.player.id === fUPO.playerId;
+    });
+  });
+
+  const allMatchingPlayersStrings = allMatchingPlayersObjects.map((mPO) => {
+    return mPO.player.firstName;
+  });
+
+  //FILTERED BASED ON BEING MENTIONED....
+  // this colleciton is ONLY UPOS for the current user that HAVE NOT been marked as mentioned
   const filteredUsersPlayers = usersPlayers.filter((upo) => {
     return (
       upo.userId === parseInt(localStorage.getItem("whpf_user")) &&
@@ -88,17 +116,19 @@ export const MessageEntryForm = () => {
     );
   });
 
-  const matchingPlayersObjects = filteredUsersPlayers.map((fUPO) => {
+  //Player Objects that correspond to the above
+  const filteredPlayersObjects = filteredUsersPlayers.map((fUPO) => {
     return playerObjArray.find((p) => {
       return p.player.id === fUPO.playerId;
     });
   });
 
-  const matchingPlayersStrings = matchingPlayersObjects.map((mPO) => {
+  //Strings of the Player first names for the above (aka only player first names in the user's lineup they haven't mentioned yet)
+  const filteredPlayersStrings = filteredPlayersObjects.map((mPO) => {
     return mPO.player.firstName;
   });
 
-  // for other player's lineups...
+  // FOR OTHER PLAYERS LINEUPS
   const othersUsersPlayers = usersPlayers.filter((upo) => {
     return upo.userId != parseInt(localStorage.getItem("whpf_user"));
   });
@@ -117,22 +147,21 @@ export const MessageEntryForm = () => {
     <section className="messageEntryForm">
       <form>
         <fieldset>
-        <button
-          onClick={(e) => {
+          <button
+            onClick={(e) => {
               e.preventDefault();
-              messagetextRef.current.value = 'I stan for '
-          }}
-        >
-          Stan by your man
-        </button>
-        <button
-          onClick={(e) => {
+              handleStanButtonPress();
+            }}
+          >
+            Stan by your man
+          </button>
+          <button
+            onClick={(e) => {
               e.preventDefault();
-              messagetextRef.current.value = `You know who's garbage? `
-          }}
-        >
-          Talk that trash
-        </button>
+            }}
+          >
+            Talk that trash
+          </button>
           <input
             type="text"
             name="messagetext"
@@ -153,7 +182,7 @@ export const MessageEntryForm = () => {
         <button
           onClick={(e) => {
             e.preventDefault();
-            handleSubmitButtonEvent();
+            // handleSubmitButtonEvent();
           }}
         >
           Get in the mix
