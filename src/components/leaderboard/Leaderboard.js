1fa587c870263b8aeadf3415b692f17b56f24951
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-17 01:46:35 -0500

    well its on the page gotta gettem hooked up

--- src/components/leaderboard/Leaderboard.js
+++ src/components/leaderboard/Leaderboard.js
@@ -1,25 +1,47 @@
 import React, { useEffect, useContext, useState } from "react";
 import { UserContext } from "../users/UserProvider";
+import { MessageContext } from "../messages/MessageProvider";
 
 export const Leaderboard = () => {
-    const { getUsers, users } = useContext(UserContext);
+  const { getUsers, users } = useContext(UserContext);
+  const { messages, getMessages } = useContext(MessageContext);
+  const [userScores, setUserScores] = useState([]);
 
   useEffect(() => {
-    getUsers();
+    getUsers().then(getMessages);
   }, []);
 
-    const userScores = users.map(u => {
-        const userScoreObj = {
+  useEffect(() => {
+    console.log("messages change!");
+    setUserScores(
+      users
+        .map((u) => {
+          const userScoreObj = {
+            username: u.name,
             userId: u.id,
-            score: u.messages.length
-        }
+            score: u.messages.length,
+          };
 
-        return userScoreObj
-    })
+          return userScoreObj;
+        })
+        .reverse()
+    );
+  }, [messages]);
 
-    return (
-        <table>
-            <tr><th>User:</th><th>Score:</th></tr>
-        </table>
-    )
+  return (
+    <table>
+      <tr>
+        <th>User:</th>
+        <th>Stans:</th>
+      </tr>
+      {userScores.map((uSO) => {
+        return (
+          <tr>
+            <td>{uSO.username}</td>
+            <td>{uSO.score}</td>
+          </tr>
+        );
+      })}
+    </table>
+  );
 };
