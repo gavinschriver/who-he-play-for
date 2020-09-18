a3c3d21ab19f9d341c86c0b0b3dd041ddf4e296a
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-17 21:11:52 -0500

    deet button working for players

--- src/components/players/player.js
+++ src/components/players/player.js
@@ -1,12 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 
 export const Player = ({ PO }) => {
+    const [showHideDetails, setShowHideDetails] = useState(false)
+
+    const handleDetailButtonClick = () => {
+        if (!showHideDetails) {
+            setShowHideDetails(true)
+        } else setShowHideDetails(false)
+    }
+
+
     return (
         <article>
-                <div>Player Name: {PO.player.firstName}</div>
+                <div className="player__card">Player Name: {PO.player.firstName}</div>
                 <div>
                   <img src={PO.player.officialImageSrc} />
-                </div>
+            </div>
+            <button onClick={(e) => {
+                e.preventDefault()
+                handleDetailButtonClick()
+            }
+            }>Get the juicy deets:</button>
+            {
+                showHideDetails
+                    ? <div>DEETS</div>
+                    : <div></div>
+            }
               </article>
     )
 }
