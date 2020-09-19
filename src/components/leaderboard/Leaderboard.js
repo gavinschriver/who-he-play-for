4229ebfdbd030975736c82b910735d12848a708c
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-18 20:05:06 -0500

    FUCK um jesus

--- src/components/leaderboard/Leaderboard.js
+++ src/components/leaderboard/Leaderboard.js
@@ -7,22 +7,24 @@ import { UserPlayerContext } from "../usersPlayers/UsersPlayersProvider";
 export const Leaderboard = () => {
   const { getUsers, users } = useContext(UserContext);
   const { messages } = useContext(MessageContext);
-  const { playerObjArray } = useContext(PlayerContext)
-  const { usersPlayers } = useContext(UserPlayerContext)
+  const { getPlayerData, playerObjArray } = useContext(PlayerContext);
+  const { getUsersPlayers, usersPlayers } = useContext(UserPlayerContext);
 
   // only messages marked as trash talk
-  const trashtalkMessages = messages.filter(m => {
-    return m.trashtalk
-  })
+  const trashtalkMessages = messages.filter((m) => {
+    return m.trashtalk;
+  });
 
-  const trashtalkStringNameInstances = trashtalkMessages.map(ttMO => {
-    return ttMO.messagetext
-  })
+  const trashtalkStringNameInstances = trashtalkMessages.map((ttMO) => {
+    return ttMO.messagetext;
+  });
 
-  console.log(trashtalkStringNameInstances)
+  console.log(trashtalkStringNameInstances);
 
   useEffect(() => {
-    getUsers();
+    getUsers()
+      .then(getUsersPlayers)
+      .then(getPlayerData);
   }, []);
 
   useEffect(() => {
@@ -30,30 +32,58 @@ export const Leaderboard = () => {
   }, [messages]);
 
   const userScores = users.map((u) => {
-    let userscore = 0
+    let userscore = 0;
 
     const userMessages = u.messages;
 
-      userMessages.forEach(m => {
-          if (m.stan) {
-            userscore = userscore + 5
-          } else if (m.trashtalk) {
-              userscore++
-        }
+    userMessages.forEach((m) => {
+      if (m.stan) {
+        userscore = userscore + 5;
+      } else if (m.trashtalk) {
+        userscore++;
+      }
     });
     const userScoreObj = {
       username: u.name,
       userId: u.id,
-      score: userscore
+      score: userscore,
     };
+
+    const What = usersPlayers
+
 
     return userScoreObj;
   });
 
-  
+  const trashedUserScores = userScores.map((uSO) => {
+    const matchingUserObject = users.find((u) => {
+      return u.id === uSO.userId;
+    });
 
+    const matchingUserPlayerObjects = usersPlayers.filter((uPO) => {
+      return uPO.userId === matchingUserObject.id || {};
+    });
 
+    const matchingPlayerObjects = matchingUserPlayerObjects.map((mUPO) => {
+      return playerObjArray.find((pO) => {
+        return pO.player.id === mUPO.playerId;
+      });
+    });
 
+    const matchingPlayerStrings = matchingPlayerObjects.map((mPO) => {
+      return mPO.player.firstName;
+    });
+
+    trashtalkStringNameInstances.forEach((ttSNI) => {
+      if (matchingPlayerStrings.includes(ttSNI)) {
+        uSO.score--;
+      }
+    });
+
+    return uSO;
+  });
+
+  console.log(trashedUserScores)
 
   const sortedScores = userScores.sort((a, b) => {
     return b.score - a.score;
@@ -62,19 +92,19 @@ export const Leaderboard = () => {
   return (
     <table>
       <tbody>
-      <tr>
-        <th>User:</th>
-        <th>Stans:</th>
-      </tr>
-      {sortedScores.map((uSO) => {
-        return (
-          <tr>
-            <td>{uSO.username}</td>
-            <td>{uSO.score}</td>
-          </tr>
-        );
-      })}
-        </tbody>
+        <tr>
+          <th>User:</th>
+          <th>Stans:</th>
+        </tr>
+        {sortedScores.map((uSO) => {
+          return (
+            <tr>
+              <td>{uSO.username}</td>
+              <td>{uSO.score}</td>
+            </tr>
+          );
+        })}
+      </tbody>
     </table>
   );
 };
