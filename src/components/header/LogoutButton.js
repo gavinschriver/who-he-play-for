3f15ac3a543ac9ed95f66f4cd3eebd40da407695
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-23 23:16:44 -0500

    alert city

--- src/components/header/LogoutButton.js
+++ src/components/header/LogoutButton.js
@@ -1,11 +1,13 @@
 import React, { useState, useContext } from "react";
 import { Redirect } from "react-router-dom";
-import { UserContext } from "../users/UserProvider"
+import { UserContext } from "../users/UserProvider";
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 
 export const LogoutButton = ({ location, user }) => {
-  const { removeUser } = useContext(UserContext)
+  const { removeUser } = useContext(UserContext);
   const [loggedOut, setLoggedOut] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
   const logout = () => {
     localStorage.removeItem("whpf_user");
@@ -14,7 +16,7 @@ export const LogoutButton = ({ location, user }) => {
 
   const logoutAndDelete = () => {
     localStorage.removeItem("whpf_user");
-    removeUser(user.id)
+    removeUser(user.id);
     setLoggedOut(true);
   };
 
@@ -24,9 +26,26 @@ export const LogoutButton = ({ location, user }) => {
 
   if (location === "navbar") {
     return <Button onClick={logout}>LogOut</Button>;
-  } 
+  }
 
   if (location === "userAccount") {
-    return <Button onClick={logoutAndDelete}>LogOut</Button>
+    return (
+      <>
+        <Button
+          onClick={() => {
+            setShowAlert(true);
+          }}
+        >
+          Delete account forever and ever
+        </Button>
+        {showAlert ? (
+          <Alert dismissible onClose={() => setShowAlert(false)}>
+            SUreee about that?<Button onClick={logoutAndDelete}>YES TERMINATE ME</Button>
+          </Alert>
+        ) : (
+          <div></div>
+        )}
+      </>
+    );
   }
 };
