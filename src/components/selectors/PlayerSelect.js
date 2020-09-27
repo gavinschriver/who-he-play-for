8a2b536d3087bd18a55c30c430e38e0063e19602
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-27 14:34:04 -0500

    seems to be making sense?

--- src/components/selectors/PlayerSelect.js
+++ src/components/selectors/PlayerSelect.js
@@ -9,21 +9,18 @@ export default (props) => {
     UserContext
   );
   const { playerObjArray, getPlayerData } = useContext(PlayerContext);
-  const { usersPlayers } = useContext(UserPlayerContext);
+  const { usersPlayers, getUsersPlayers } = useContext(UserPlayerContext);
 
   const [currentUser, setCurrentUser] = useState({
     usersPlayers: [],
     messages: [],
   });
   const [currentUsersPlayers, setCurrentUsersPlayers] = useState([]);
+  const [otherUsersPlayers, setOtherUsersPlayers] = useState([]);
 
   const filteredUsersPlayers = currentUser.usersPlayers.filter(
     (up) => !up.mentioned
   );
-
-  useEffect(() => {
-    getUserById(currentUserId).then(setCurrentUser).then(getPlayerData);
-  }, []);
 
   useEffect(() => {
     const matchingPlayers =
@@ -34,17 +31,34 @@ export default (props) => {
       }) || {};
     setCurrentUsersPlayers(matchingPlayers);
   }, [playerObjArray]);
+    
+    useEffect(() => {
+        const allOtherUsersPlayers = usersPlayers.map((up) => {
+            return playerObjArray.find(p => {
+                return p.player.id === up.playerId
+            })
+        }) || {}
+        setOtherUsersPlayers(allOtherUsersPlayers)
+    }, [playerObjArray])
 
-  //need this to control for whenever a player is marked as mentioned
+  //need this to control for whenever a current user's player is marked as mentioned
   useEffect(() => {
     getUserById(currentUserId).then(setCurrentUser);
   }, [usersPlayers]);
 
+  // init hook
+  useEffect(() => {
+    getUserById(currentUserId)
+      .then(setCurrentUser)
+      .then(getPlayerData)
+      .then(getUsersPlayers);
+  }, []);
+
   return (
     <FormControl as="select">
-      {currentUsersPlayers.map((p) => (
-        <option>{p.player.firstName}</option>
-      ))}
+          {otherUsersPlayers.map((p) => (
+              <option>{p.player.firstName}</option>
+          ))}
     </FormControl>
   );
 };
