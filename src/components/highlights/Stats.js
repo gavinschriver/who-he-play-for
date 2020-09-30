f76574e10888290a49c4dd79757733678aa9d0ef
Gavin Schriver <alexanderschriver@gmail.com>
2020-09-29 19:24:41 -0500

    fuck off i htink its working hahaha

--- src/components/highlights/Stats.js
+++ src/components/highlights/Stats.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
 
 export default (props) => {
   const [data, setData] = useState({});
+  const [careerStats, setCareerStats] = useState({});
 
   useEffect(() => {
     fetch(`http://data.nba.net/prod/v1/2019/players/${props.id}_profile.json`)
@@ -10,15 +12,30 @@ export default (props) => {
         setData(result.league.standard.stats);
       });
   }, []);
-    
-    useEffect(() => {
-       console.log(data) 
-    }, [data])
 
+  useEffect(() => {
+    setCareerStats(data.careerSummary || {});
+  }, [data]);
+    
+    console.log(careerStats)
 
   return (
-    <article>
-      <h2>YAR</h2>
-    </article>
+    <Table>
+      <thead>
+        <tr>
+          <th>Stat sample</th>
+          <th>Points per Game</th>
+          <th>Assists per Game</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>All-time</td>
+          <td>{careerStats.ppg}</td>
+          <td>EVEN MORE</td>
+        </tr>
+      </tbody>
+    </Table>
   );
 };
