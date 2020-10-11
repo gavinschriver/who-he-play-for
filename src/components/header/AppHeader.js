c78440fc4fc1d9fb3b491412adf9fd754d7612c6
Gavin Schriver <alexanderschriver@gmail.com>
2020-10-11 15:09:01 -0500

    adding instruction text to login page, trying to figure out mobile layout danit

--- src/components/header/AppHeader.js
+++ src/components/header/AppHeader.js
@@ -28,7 +28,6 @@ export const AppHeader = (props) => {
     <article className="appHeader">
       <Row>
         <Col>
-          <SiteTitle />
           <UserGreeting user={user} />
         </Col>
         <Col>
